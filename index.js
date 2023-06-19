17d3e42157e350f7beb05ca457944a2b60583f5a
njugudev <njugudev@pop-os.localdomain>
2023-06-19 19:09:07 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+const input = document.getElementById('input');
+
 function addingEventListener() {
+    input.addEventListener('click', function() {
+        alert('I was clicked!');
+      });
 }
+
+
+
+input.addEventListener('click', addingEventListener);
+
+
+
+
+
+
+
+
